792f0b7630471bd2e9aacbae91d78d5c24e46912
XOIAN777 <binario555@gmail.com>
2025-07-28 23:53:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "¡Hola! Soy tu Kaián privado en la nube. 🌌"
+    return """
+    <html>
+    <head>
+        <title>Kaián en la Nube</title>
+    </head>
+    <body style='background-color: #f0f0ff; text-align: center; padding-top: 100px; font-family: sans-serif;'>
+        <h1>🌺 ¡Hola! Soy tu Kaián privado en la nube. ☁️</h1>
+        <p style='font-size: 20px;'>Con música de fondo... 🎶</p>
+        <iframe width='0' height='0' src='https://www.youtube.com/embed/l3O4GzQp2aQ?autoplay=1&loop=1&playlist=l3O4GzQp2aQ'
+            frameborder='0' allow='autoplay' allowfullscreen></iframe>
+    </body>
+    </html>
+    """
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
